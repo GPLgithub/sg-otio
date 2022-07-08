0b297c61f12706fc983e2247815fedecd5b26912
Kevin Sallee <kevin.sallee@gpltech.com>
2022-07-08 12:49:02 -0500

    removing python 3.8 for now

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,9 +12,6 @@ from sg_otio.constants import _SG_OTIO_MANIFEST_PATH
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Set the root logger to DEBUG for the tests
-logging.getLogger().setLevel(logging.DEBUG)
-
 sys.path.append(os.path.dirname(__file__))
 
 
--- tests/test_media_cutter.py
+++ tests/test_media_cutter.py
@@ -61,11 +61,9 @@ class TestMediaCutter(unittest.TestCase):
             self.assertEqual(clip.media_reference.name, media_names[i])
             self.assertEqual(os.path.basename(clip.media_reference.target_url), file_names[i])
             media_filepath = clip.media_reference.target_url.replace("file://", "")
-            # self.assertTrue(os.path.isfile(media_filepath), msg="{} does not exist".format(media_filepath))
+            self.assertTrue(os.path.isfile(media_filepath), msg="{} does not exist".format(media_filepath))
             # Fourth entry is a dummy reference to "foo.mov"
-            # TODO: somehow certain platforms/python versions delete the files before this runs...
-            #       fix it!
-            if ffprobe and i != 4 and os.path.isfile(media_filepath):
+            if ffprobe and i != 4:
                 # ffprobe  -count_frames -show_entries stream=nb_read_frames -v error  -print_format csv
                 cmd = [
                     "ffprobe",
